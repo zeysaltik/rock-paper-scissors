f0f90d876376ef0abc1fd91cf2bd23d15d22331a
zeysaltik <zeysaltik200@gmail.com>
2024-08-13 17:54:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const gameContainer = document.querySelector(".container"),
+userResult = document.querySelector(".user_result"),
+cpuResult = document.querySelector(".cpu_result"),
+result = document.querySelector(".result"),
+optionImages = document.querySelector(".option_images")
+
+optionImages.forEach((image, index) => {
+    addEventListener("click", () => {
+        image.classList
+    })
+})
